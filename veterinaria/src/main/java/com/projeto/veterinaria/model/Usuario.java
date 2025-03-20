443639bc5499e114cb38699065f6b24f463100fc
Felipe Seiji <fenishina@gmail.com>
2025-03-20 17:52:45 -0300

    Criada classe Usuario

---
+++ veterinaria/src/main/java/com/projeto/veterinaria/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.projeto.veterinaria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String telefone;
+    private String email;
+}
